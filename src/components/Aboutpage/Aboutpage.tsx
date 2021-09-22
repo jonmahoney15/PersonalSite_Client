3be39a5bdfe8223d9506fcdaabe613189e4f6c8a
jonmahoney15 <jonmahoney15@yahoo.com>
2021-09-22 16:16:48 -0400

    hopefully fixes token issue

--- src/components/Aboutpage/Aboutpage.tsx
+++ src/components/Aboutpage/Aboutpage.tsx
@@ -1,8 +1,28 @@
 import Cards from './Cards';
 import Skills from './Skills';
 import Bio from './Bio';
+import {useEffect} from 'react';
+import useToken from '../Common/useToken';
+import api from '../../api/api';
 
 const Aboutpage = () => {
+  const {token, setToken} = useToken();
+
+  useEffect(() => {
+    const getToken = async () => {
+      await api.get('/api/auth/token').then(response => {
+        setToken(response.data.token);
+        api.defaults.headers.common['x-auth-token'] = token;
+      }).catch(error => {
+        console.log(error);
+      });
+    }
+
+    if (!token) {
+      getToken();
+    }
+    // eslint-disable-next-line
+  }, [])
   return (
     <div className="flex flex-col h-screen">
       <div className="flex flex-col bg-cover bg-gradient-to-b from-black to-purple-500">
--- src/components/Common/useToken.ts
+++ src/components/Common/useToken.ts
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 const useToken = () => {
   const getToken = () => {
-    return sessionStorage.getItem('auth-token')
+    return sessionStorage.getItem('auth-token');
   };
 
   const [token, setToken] = useState(getToken());
