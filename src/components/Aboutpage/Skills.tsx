6c777519aa1ead27219b8d542d3a069686498024
jonmahoney15 <jonmahoney15@yahoo.com>
2021-10-28 12:15:48 -0400

    Added a tooltip and some more images

--- src/components/Aboutpage/Skills.tsx
+++ src/components/Aboutpage/Skills.tsx
@@ -1,19 +1,30 @@
+import ToolTipImage from "../Common/ToolTipImage";
 const Skills = () => {
     return (
-    <div className="flex jusitfy-center p-5 items-center flex-col">
-        <h3 className="jusitfy-center text-white mt-5 text-5xl">Skills:</h3>
-        <div className="flex flex-row justify-evenly items-center p-10">
-            <img src={process.env.PUBLIC_URL+"/svg/react-2.svg"} alt="React" className="m-1 md:m-10" style={{height: 120}}/>
-            <img src={process.env.PUBLIC_URL+"/svg/nodejs-icon.svg"} alt="Nodejs" className="m-1 md:m-10" style={{height: 120}}/>
-            <img src={process.env.PUBLIC_URL+"/svg/c.svg"} alt="C++" className="m-1 md:m-10" style={{height: 120}}/>
-            <img src={process.env.PUBLIC_URL+"/svg/docker-vector-logo.svg"} alt="Docker" className="m-1 md:m-10" style={{height: 120}}/>
+    <div className="flex flex-col items-center p-5 jusitfy-center">
+        <h3 className="mt-5 text-5xl text-white">Languages, tools and frameworks:</h3>
+        <div className="flex flex-col items-center p-10 justify-evenly">
+            <div className="flex flex-row items-center justify-evenly">
+                <ToolTipImage color="gray" Image="/svg/react-2.svg" AltText="React" Title="React" content="I have used react for a few years now. This site is built with react and typescript"/>
+                <ToolTipImage color="gray" Image="/svg/nodejs-icon.svg" AltText="Nodejs" Title="Nodejs" content="Nodejs is one of my favorite backends. I use it for anything I am trying to quickly prototype."/>
+                <ToolTipImage color="gray" Image="/svg/c.svg" AltText="C" Title="C" content="I learned how to program with C. Now I am trying to contribute to the open source firmware Betaflight which is written in C."/>
+                <ToolTipImage color="gray" Image="/svg/c_plus_plus.svg" AltText="C++" Title="C++" content="I worked for a year writing C++ for flight software. It is a great language, and I am very comfortable using it"/>
+            </div>
+            <div className="flex flex-row items-center justify-evenly">
+                <ToolTipImage color="gray" Image="/svg/git.svg" AltText="Git" Title="Git" content="Git is my prefered version control. It is a great tool and I hate UI, terminal forever."/>
+                <ToolTipImage color="gray" Image="/svg/gcp.svg" AltText="GCP" Title="Google Cloud Platform" content="I use GCP a lot. Though I don't think its anything different from AWS or AZURE."/>
+                <ToolTipImage color="gray" Image="/svg/firebase.svg" AltText="Firebase" Title="Firebase" content="Firebase is a cool extension of GCP. Makes deploying simple sites very quick."/>
+                <ToolTipImage color="gray" Image="/svg/docker.svg" AltText="Docker" Title="Docker" content="Another tool I use often is Docker. Containers are the future. It speeds up development environement and makes production portable."/>
+                <ToolTipImage color="gray" Image="/svg/vim.svg" AltText="Vim" Title="Vim" content="Vim is my favorite editor. The custimization is awesome. I like being able to stay in the terminal. It also is not very resource intensive so my old macbook with 4GB of RAM can still be very useful."/>
+            </div> 
+            
         </div>
-        <div className="flex flex-col text-white text-5xl justify-center align-middle">
+        <div className="flex flex-col justify-center text-5xl text-white align-middle">
             <p>Check out some of my work: </p>
             <div className="p-5">
                 <a href="https://github.com/jonmahoney15/">
-                    <button className="flex item-center w-full">
-                        <img src={process.env.PUBLIC_URL+"/svg/github.svg"} alt="gihub" className="w-full" style={{height: 100}}/>
+                    <button className="flex justify-center w-full item-center">
+                        <ToolTipImage color="gray" Image="/svg/github.svg" AltText="Github" Title="My Github Account!" content=""/>
                     </button>
                 </a>
             </div>
---
+++ src/components/Common/ToolTipImage.tsx
@@ -0,0 +1,71 @@
+import React, { useRef } from "react";
+import { createPopper } from "@popperjs/core";
+
+interface ITooltipProps {
+    color: string;
+    Image: string;
+    AltText: string;
+    Title: string;
+    content: string;
+}
+
+const TooltipImage = (props: ITooltipProps) => {
+  const [tooltipShow, setTooltipShow] = React.useState(false);
+  const btnRef = useRef();
+  const tooltipRef = useRef();
+  const openLeftTooltip = () => {
+    //@ts-ignore
+    createPopper(btnRef.current, tooltipRef.current, {
+      placement: "top"
+    });
+    setTooltipShow(true);
+  };
+  const closeLeftTooltip = () => {
+    setTooltipShow(false);
+  };
+  return (
+    <>
+      <div className="flex flex-wrap">
+        <div className="w-full text-center">
+          <img
+            src={process.env.PUBLIC_URL+props.Image}
+            alt={props.AltText}
+            className="m-1 md:m-10"
+            style={{height: 80}}
+            onMouseEnter={openLeftTooltip}
+            onMouseLeave={closeLeftTooltip}
+            //@ts-ignore 
+            ref={btnRef}
+          />
+          <div
+            className={
+              (tooltipShow ? "" : "hidden ") +
+              "bg-" +
+              props.color +
+              "-600 border-0 mb-3 block z-50 font-normal leading-normal text-sm max-w-xs text-left no-underline break-words rounded-lg"
+            }
+            //@ts-ignore
+            ref={tooltipRef}
+          >
+            <div>
+              <div
+                className={
+                  "bg-" +
+                  props.color +
+                  "-600 text-white opacity-75 font-semibold p-3 mb-0 border-b border-solid border-blueGray-100 uppercase rounded-t-lg"
+                }
+              >
+                {props.Title}
+              </div>
+              <div className="p-3 text-white">
+                  {props.content}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TooltipImage;
